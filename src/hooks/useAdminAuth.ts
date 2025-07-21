3fa7b1b486d528ee5cecd353a33c005445edf70c
Mahmudul Sakib <mahmudulsakib.110@gmail.com>
2025-07-21 16:54:32 +0600

    added

--- src/hooks/useAdminAuth.ts
+++ src/hooks/useAdminAuth.ts
@@ -1,38 +1,4 @@
-// "use client";
-
-// import { useEffect, useState } from "react";
-// import { useRouter } from "next/navigation";
-// import axios from "axios";
-
-// export default function useAdminAuth() {
-//   const router = useRouter();
-//   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-//   const [loading, setLoading] = useState<boolean>(true);
-
-//   useEffect(() => {
-//     const checkAuth = async () => {
-//       try {
-//         await axios.get(
-//           "https://taskora-admin-backend.onrender.com/admin/protected-data",
-//           {
-//             withCredentials: true,
-//           }
-//         );
-
-//         setIsAuthenticated(true);
-//       } catch (error) {
-//         router.push("/");
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-
-//     checkAuth();
-//   }, [router]);
-
-//   return { isAuthenticated, loading };
-// }
-
+// /hooks/useAdminAuth.ts
 "use client";
 
 import { useEffect, useState } from "react";
@@ -47,7 +13,7 @@ export default function useAdminAuth() {
   useEffect(() => {
     axios
       .get("https://taskora-admin-backend.onrender.com/admin/me", {
-        withCredentials: true,
+        withCredentials: true, // ✅ ensure cookies go with the request
       })
       .then((res) => {
         if (res.data?.admin) {
@@ -57,7 +23,7 @@ export default function useAdminAuth() {
         }
       })
       .catch(() => {
-        router.push("/admin-login");
+        router.push("/admin-login"); // ⛔️ cookie not present = redirect
       })
       .finally(() => {
         setLoading(false);
