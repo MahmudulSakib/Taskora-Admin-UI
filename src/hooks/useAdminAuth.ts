bbc3ccda9a623fec7fdf3358cc1ca238b2021dc6
Mahmudul Sakib <mahmudulsakib.110@gmail.com>
2025-07-21 16:36:11 +0600

    added

--- src/hooks/useAdminAuth.ts
+++ src/hooks/useAdminAuth.ts
@@ -1,3 +1,38 @@
+// "use client";
+
+// import { useEffect, useState } from "react";
+// import { useRouter } from "next/navigation";
+// import axios from "axios";
+
+// export default function useAdminAuth() {
+//   const router = useRouter();
+//   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+//   const [loading, setLoading] = useState<boolean>(true);
+
+//   useEffect(() => {
+//     const checkAuth = async () => {
+//       try {
+//         await axios.get(
+//           "https://taskora-admin-backend.onrender.com/admin/protected-data",
+//           {
+//             withCredentials: true,
+//           }
+//         );
+
+//         setIsAuthenticated(true);
+//       } catch (error) {
+//         router.push("/");
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+
+//     checkAuth();
+//   }, [router]);
+
+//   return { isAuthenticated, loading };
+// }
+
 "use client";
 
 import { useEffect, useState } from "react";
@@ -6,28 +41,27 @@ import axios from "axios";
 
 export default function useAdminAuth() {
   const router = useRouter();
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const checkAuth = async () => {
-      try {
-        await axios.get(
-          "https://taskora-admin-backend.onrender.com/admin/protected-data",
-          {
-            withCredentials: true,
-          }
-        );
-
-        setIsAuthenticated(true);
-      } catch (error) {
-        router.push("/");
-      } finally {
+    axios
+      .get("https://taskora-admin-backend.onrender.com/admin/me", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        if (res.data?.admin) {
+          setIsAuthenticated(true);
+        } else {
+          router.push("/admin-login");
+        }
+      })
+      .catch(() => {
+        router.push("/admin-login");
+      })
+      .finally(() => {
         setLoading(false);
-      }
-    };
-
-    checkAuth();
+      });
   }, [router]);
 
   return { isAuthenticated, loading };
